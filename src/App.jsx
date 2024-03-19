8d3c4688503ccc52d4d861719a2a71b9edb96da9
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-03-20 01:08:38 +0530

    fixed to do app again

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id ?todo :prevTodo)))
   }
   const deleteTodo=(id)=>{
-    setTodos((prev)=>prev.filter((prevTodo)=>prevTodo!==id)) 
+    setTodos((prev)=>prev.filter((prevTodo)=>prevTodo.id!==id)) 
   }
   const toggleComplete=(id)=>{
     setTodos((prev)=>prev.map((prevTodos)=>prevTodos.id===id ?{...prevTodos,completed:!prevTodos.completed} :prevTodos))
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos,addTodo,deleteTodo,toggleComplete,updatedTodo}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172842] min-h-screen py-8 w-full">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
